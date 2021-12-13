09539564bbd341c385d7507a006e450f9bd14545
Joaby Oliveira <joabyoliveira2004@gmail.com>
2021-12-13 08:20:04 -0300

    learn: create procedures with params

--- procedures.sql
+++ procedures.sql
@@ -1,3 +1,4 @@
+/* Procedures without params */
 delimiter $
 
 create procedure nome_empresa()
@@ -7,3 +8,14 @@ end
 $
 
 CALL nome_empresa()$
+
+
+/* Procedures with params */
+
+create procedure somar(n1 INT, n2 INT)
+begin
+  select n1 + n2 as conta;
+end
+$
+
+CALL somar(1, 70)$
